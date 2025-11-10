9738065ae3408fd574c5b01e147588e75e9bfa27
Maikon Bueno <maikon@gmail.com>
2025-11-10 10:37:26 -0400

    adicionando pagina de primeiro acesso e pagina de cadastro de projeto

--- app/projetos/primeiro-acesso/page.tsx
+++ app/projetos/primeiro-acesso/page.tsx
@@ -1,7 +1,110 @@
-export default function Page() {
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Controller, useForm } from "react-hook-form"
+import { toast } from "sonner"
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(3, "Username must be at least 3 characters.")
+    .max(10, "Username must be at most 10 characters.")
+    .regex(
+      /^[a-zA-Z0-9_]+$/,
+      "Username can only contain letters, numbers, and underscores."
+    ),
+})
+
+export default function FormRhfInput() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    toast("You submitted the following values:", {
+      description: (
+        <pre className="bg-code text-code-foreground mt-2 w-[320px] overflow-x-auto rounded-md p-4">
+          <code>{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+      position: "bottom-right",
+      classNames: {
+        content: "flex flex-col gap-2",
+      },
+      style: {
+        "--border-radius": "calc(var(--radius)  + 4px)",
+      } as React.CSSProperties,
+    })
+  }
+
   return (
-    <div className="p-4 bg-muted/50 aspect-video rounded-xl">
-      <h1>PRIMEIRO ACESSO</h1>
-    </div>  
-  );
+    <Card className="w-full sm:max-w-md">
+      <CardHeader>
+        <CardTitle>Configurações do Perfil</CardTitle>
+        <CardDescription>
+          Atualize seus dados para facilitar os demais cadastros.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form id="form-rhf-input" onSubmit={form.handleSubmit(onSubmit)}>
+          <FieldGroup>
+            <Controller
+              name="username"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="form-rhf-input-username">
+                    Nome Completo do Proponente
+                  </FieldLabel>
+                  <Input
+                    {...field}
+                    id="form-rhf-input-username"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Nome Completo"
+                    autoComplete="nome"
+                  />
+                  <FieldDescription>
+                    Este nome será utilizado para preenchimento
+                    de todas as documentações.
+                  </FieldDescription>
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+        </form>
+      </CardContent>
+      <CardFooter>
+        <Field orientation="horizontal">
+          <Button type="submit" form="form-rhf-input">
+            Salvar
+          </Button>
+        </Field>
+      </CardFooter>
+    </Card>
+  )
 }
